403d9073a2704e911c9d492b74bcd3ca96a4b1a8
Michael Böhme <boehme.mic@gmail.com>
2020-10-16 00:07:04 +0200

    updated URL for EF

--- .github/workflows/download_EF.py
+++ .github/workflows/download_EF.py
@@ -9,7 +9,7 @@ DATAPATH = os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep +
 DATE_STR = datetime.fromtimestamp(datetime.now().timestamp()).strftime('%Y-%m-%d')
 FILENAME = "EF_{}.html".format(DATE_STR)
 FULLNAME = DATAPATH + FILENAME
-URL  = "https://www.erfurt.de/ef/de/service/aktuelles/topthemen/2020/134840.html"
+URL  = "http://www.erfurt.de/coronavirus"
 
 if os.path.isfile(FULLNAME):
 
